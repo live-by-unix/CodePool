9bb63df8e46f76b69d2f10088474974c81eb12d7
live-by-unix <abhayscontact@gmail.com>
2026-04-18 15:14:29 -0700

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
@@ -10,4 +11,4 @@ export default clerkMiddleware(async (auth, request) => {
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+}; 
